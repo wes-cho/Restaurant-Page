92e3c52b082c7e0f56c6e8471e72d7ff9a5273c0
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-09-16 15:43:32 -0400

    Convert HTML elements to be created using DOM

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader",
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },      
     ],
   },
 };
