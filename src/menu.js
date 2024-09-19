97c3fc41d1f8afccb8128c19d91f3773543ecff2
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-09-19 19:00:05 -0400

    Changed primary styling of website

--- src/menu.js
+++ src/menu.js
@@ -23,5 +23,19 @@ function loadMenu(){
         const description = document.createElement('p');
             description.textContent = "A shot of espresso. Plain and simple.";
             menuItem.appendChild(description);
+    
+    const menuItem1 = document.createElement("div");
+    menuBox.appendChild(menuItem1);
+        menuItem1.setAttribute('class', 'menu-item');
+        menuItem1.setAttribute('id', 'cappuccino');
+        const title1 = document.createElement("h2");
+            title1.textContent = 'Cappuccino';
+            menuItem1.appendChild(title1);
+        const price1 = document.createElement('p');
+            price1.textContent = "$2.75";
+            menuItem1.appendChild(price1);
+        const description1 = document.createElement('p');
+            description1.textContent = "Espresso, milk, and foam. Perfectly balanced as all things should be.";
+            menuItem1.appendChild(description1);
 }
 
