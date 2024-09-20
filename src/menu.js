112a30e7e098eb9f700db80753b3a57121f736a9
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-09-19 20:00:11 -0400

    Dynamically generate menu item card

--- src/menu.js
+++ src/menu.js
@@ -2,14 +2,13 @@ export {loadMenu};
 
 function loadMenu(){
     const header = document.createElement("h1");
-    header.setAttribute("id", "restaurant-header");
-    header.textContent = "Menu"
+        header.setAttribute("id", "restaurant-header");
+        header.textContent = "Menu"
     content.appendChild(header);
 
-    const menuBox = document.createElement("div");
-        menuBox.setAttribute("id", "menu");
-        content.appendChild(menuBox);
-
+    const menuContainer = document.createElement("div");
+        menuContainer.setAttribute("id", "menu");
+    content.appendChild(menuContainer);
 
     const menu = [
         {
@@ -41,37 +40,25 @@ function loadMenu(){
             title: 'Seasonal',
             price: '$6.75',
             description: 'Limited-time special, unlimited happiness.',
-        },
+        }
     ];
 
-    const menuItem = document.createElement("div");
-    menuBox.appendChild(menuItem);
-        menuItem.setAttribute('class', 'menu-item');
-        menuItem.setAttribute('id', 'espresso');
-        const title = document.createElement("h2");
-            title.textContent = 'Espresso';
-            menuItem.appendChild(title);
-        const price = document.createElement('p');
-            price.textContent = "$2.75";
-            menuItem.appendChild(price);
-        const description = document.createElement('p');
-            description.textContent = "A shot of espresso. Plain and simple.";
-            menuItem.appendChild(description);
-    
-    const menuItem1 = document.createElement("div");
-    menuBox.appendChild(menuItem1);
-        menuItem1.setAttribute('class', 'menu-item');
-        menuItem1.setAttribute('id', 'cappuccino');
-        const title1 = document.createElement("h2");
-            title1.textContent = 'Cappuccino';
-            menuItem1.appendChild(title1);
-        const price1 = document.createElement('p');
-            price1.textContent = "$2.75";
-            menuItem1.appendChild(price1);
-        const description1 = document.createElement('p');
-            description1.textContent = "Espresso, milk, and foam. Perfectly balanced as all things should be.";
-            menuItem1.appendChild(description1);
-}
+    for (let item=0; item<menu.length; item++){
+        const itemCard = document.createElement("div");
+            itemCard.setAttribute('class', 'menu-item');
+            itemCard.setAttribute('id', `${menu[item].title.toLowerCase()}`);
+            const title = document.createElement('h2');
+                title.textContent = menu[item].title;
+                itemCard.appendChild(title);
+            const price = document.createElement('p');
+                price.textContent = menu[item].price;
+                itemCard.appendChild(price);
+            const description = document.createElement('p');
+                description.textContent = menu[item].description;
+                itemCard.appendChild(description);
+        menuContainer.appendChild(itemCard);     
+    };
+;}
 
 
 
